5b23b7023a321ffb53d76bc9fa6a21e5cebf7d83
Thomas Jakway <tjakway@gmail.com>
2015-02-19 12:16:29 -0500

    added defines for bad extensions

--- include/MusicReader.h
+++ include/MusicReader.h
@@ -10,6 +10,8 @@ extern "C" {
 
 //use an int to store codec type instead of an enum because we need to know the byte offsets in the struct and enums are technically not always ints
 //see http://stackoverflow.com/questions/1113855/is-the-sizeofenum-sizeofint-always
+#define NO_EXTENSION -1
+#define UNKNOWN_EXTENSION -2
 #define MP3_CODEC 1
 #define FLAC_CODEC 2
 #define OGG_VORBIS_CODEC 3
