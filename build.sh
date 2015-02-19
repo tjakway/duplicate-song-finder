863408af25899daa4931ae8b200385b2c5355d69
Thomas Jakway <tjakway@gmail.com>
2015-02-19 18:23:43 -0500

    added cabal build command at end

--- build.sh
+++ build.sh
@@ -5,4 +5,5 @@ HERE="$(pwd)"
 cmake -GUnix\ Makefiles . &&
 make &&
 #FIXME: don't use absolute paths!
-cabal configure --extra-include-dirs="$HERE"/include/ --extra-lib-dirs="$HERE"
+cabal configure --extra-include-dirs="$HERE"/include/ --extra-lib-dirs="$HERE" &&
+cabal build
