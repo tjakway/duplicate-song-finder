1d275bf1ff19e65e505de20526b96977ed317d7e
Thomas Jakway <tjakway@gmail.com>
2015-02-19 18:18:07 -0500

    added simple build script to synchronize cabal and CMake

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+HERE="$(pwd)"
+
+cmake -GUnix\ Makefiles . &&
+make &&
+#FIXME: don't use absolute paths!
+cabal configure --extra-include-dirs="$HERE"/include/ --extra-lib-dirs="$HERE"
